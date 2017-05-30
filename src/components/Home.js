d79bb48ff5311dabf5cc49d42b5caaaebf1b4ed8
joshuahoegen <joshuahoegen@l000179.local>
2017-05-30 08:35:38 -0400

    Initial POC is working.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import GreenScreen from "./GreenScreen/index";
+
+console.log(GreenScreen);
 
 // Home page component
 export default class Home extends React.Component {
@@ -6,6 +9,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="page-home">
+        <GreenScreen />
         <h4>Hello world!</h4>
       </div>
     );
