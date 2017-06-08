8d708b453724b2df297d7512be69b9dae4acb019
joshuahoegen <joshuahoegen@l000179.local>
2017-06-08 07:01:23 -0400

    Added get params for color picker.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import GreenScreen from "./GreenScreen/index";
 
-console.log(GreenScreen);
-
-// Home page component
 export default class Home extends React.Component {
-  // render
+  componentWillMount(){
+    this.query = this.props.location.query;
+  }
+
   render() {
+    var query = this.query;
+    console.log(query);
     return (
       <div className="page-home">
-        <GreenScreen />
-        <h4>Hello world!</h4>
+        <GreenScreen hex={query.hex || "2ab050"} />
       </div>
     );
   }
