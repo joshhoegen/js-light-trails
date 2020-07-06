9b061b5d1fa62aa051c354112e11a99c42d785cd
joshhoegen <joshhoegen@gmail.com>
2020-07-06 14:06:21 +0200

    Fixing Safari issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,55 @@
-// TODO: Fix no-no disables below.
 module.exports = {
-    "env": {
-      "browser": true
-    },
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+
+  parser: 'babel-eslint',
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    camelcase: 'off',
+    'no-param-reassign': ['error', { props: false }],
+    indent: 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-unused-vars': ['error', { varsIgnorePattern: 'dom' }],
+    'semi-style': 'off',
+    'comma-dangle': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/media-has-caption': 'off',
+    'object-curly-newline': 'off',
+    'newline-after-var': ['error', 'always'],
+    'react/button-has-type': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/sort-comp': [
+      'error',
+      {
+        order: ['lifecycle', 'everything-else', 'rendering', 'static-methods'],
+        groups: {
+          rendering: ['render', '/^render.+$/'],
+        },
+      },
     ],
-    "rules": {
-      "import/prefer-default-export": 0,
-      "no-bitwise": 0,
-      "no-plusplus": 0,
-      "no-unused-expressions": 0,
-      "react/prop-types": 0,
-      "react/jsx-filename-extension": 0,
-      "react/jsx-no-bind": 0,
-      "react/prefer-stateless-function": 0,
-      "react/jsx-tag-spacing": 0,
-      "react/jsx-space-before-closing": 0
-    }
-};
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/no-access-state-in-setstate': 'off',
+    semi: [2, 'never'],
+    'no-unused-expressions': [
+      1,
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+  },
+
+  settings: {
+    react: {
+      pragma: 'dom', // Pragma to use, default to 'React'
+    },
+  },
+}
--- src/components/trails/index.js
+++ src/components/trails/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ColorPickerPanel from 'rc-color-picker';
-import 'rc-color-picker/assets/index.css';
+import React from 'react'
+import ColorPickerPanel from 'rc-color-picker'
+import 'rc-color-picker/assets/index.css'
 // import ReactDOM from 'react-dom';
-import { Trails } from '../../lib/video-trails/Trails';
+import { Trails } from '../../lib/video-trails/Trails'
 
 export default class GreenScreen extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    const isHex = /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(this.props.hex);
-    const prefix = '#';
-    let hex = isHex === true ? this.props.hex : '#2ab050';
+    const isHex = /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(this.props.hex)
+    const prefix = '#'
+    let hex = isHex === true ? this.props.hex : '#2ab050'
 
     if (hex.substr(0, prefix.length) !== prefix) {
-      hex = prefix + hex;
+      hex = prefix + hex
     }
 
     this.state = {
@@ -25,152 +25,178 @@ export default class GreenScreen extends React.Component {
       cycle: false,
       colorize: false,
       mosaic: false,
-    };
+    }
   }
 
   componentDidMount() {
-    this.setupTrails();
+    const ua = navigator.userAgent.toLowerCase()
+    const is_safari = ua.indexOf('safari/') > -1 && ua.indexOf('chrome') < 0
+
+    if (is_safari) {
+      const video = document.getElementById('video')
+
+      setTimeout(() => {
+        video.play()
+      }, 50)
+    }
+    this.setupTrails()
   }
 
   setupTrails(camera) {
-    const rgb = this.hexToRgbA(this.state.color) || [42, 176, 80];
+    const rgb = this.hexToRgbA(this.state.color) || [42, 176, 80]
 
     if (this.trails) {
-      this.trails.stopDraw();
+      this.trails.stopDraw()
     }
 
-    this.trails = new Trails(rgb, parseInt(camera) || 0);
-    this.trails.pixelSize = this.state.size;
-    this.trails.mode = this.state.pixelate === true ? 'pixelate' : 'blur';
-    this.time = 0;
-    this.trails.selectedR = rgb[0];
-    this.trails.selectedG = rgb[1];
-    this.trails.selectedB = rgb[2];
-    this.trails.draw();
-
-    console.log(this.trails.mode);
+    this.trails = new Trails(rgb, parseInt(camera) || 0)
+    this.trails.pixelSize = this.state.size
+    this.trails.mode = this.state.pixelate === true ? 'pixelate' : 'blur'
+    this.time = 0
+    this.trails.selectedR = rgb[0]
+    this.trails.selectedG = rgb[1]
+    this.trails.selectedB = rgb[2]
+    this.trails.draw()
 
     this.trails.cameraList.then((d) => {
       this.setState({
         cameraList: d,
         activeCamera: camera,
-      });
-    });
+      })
+    })
   }
 
   changeCamera(event) {
-    const camera = event.target.value;
-    this.trails.removeCanvases();
-    console.log(this.state.size);
-    this.setupTrails(camera);
+    const camera = event.target.value
+    this.trails.removeCanvases()
+    this.setupTrails(camera)
   }
 
   // Add to utils
   hexToRgbA(hex) {
-    let c;
-    let rgb = [];
+    let c
+    let rgb = []
     if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-      c = hex.substring(1).split('');
+      c = hex.substring(1).split('')
       if (c.length === 3) {
-        c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        c = [c[0], c[0], c[1], c[1], c[2], c[2]]
       }
-      c = `0x${c.join('')}`;
-      rgb = [(c >> 16) & 255, (c >> 8) & 255, c & 255];
+      c = `0x${c.join('')}`
+      rgb = [(c >> 16) & 255, (c >> 8) & 255, c & 255]
     }
-    return rgb;
+    return rgb
   }
 
   it(time) {
     if (time > 32) {
-      return 32;
+      return 32
     }
-    return (time += 1);
+    return (time += 1)
   }
 
   cycle(event) {
-    const rgb = this.hexToRgbA(this.state.color);
+    const rgb = this.hexToRgbA(this.state.color)
     this.setState({ cycle: event.target.checked }, () => {
-      this.changeColor(rgb);
-    });
+      this.changeColor(rgb)
+    })
   }
 
   cycleColor(r, g, b) {
-    const frequency = 2.4; // .3
-    const t = this.time;
-    const center = 100;
-    const width = 60;
+    const frequency = 2.4 // .3
+    const t = this.time
+    const center = 100
+    const width = 60
 
-    r = Math.round(Math.sin(frequency * t + 0) * width + center);
-    g = Math.round(Math.sin(frequency * t + 2) * width + center);
-    b = Math.round(Math.sin(frequency * t + 4) * width + center);
+    r = Math.round(Math.sin(frequency * t + 0) * width + center)
+    g = Math.round(Math.sin(frequency * t + 2) * width + center)
+    b = Math.round(Math.sin(frequency * t + 4) * width + center)
 
-    this.time = this.it(this.time);
+    this.time = this.it(this.time)
     // console.log(`%c${r}${g}${b}`, `color: rgba(${r}, ${g}, ${b}, 1)`);
-    return [r, g, b];
+    return [r, g, b]
   }
 
   colorPicker(colorObject) {
-    const rgb = this.hexToRgbA(colorObject.color);
-    this.changeColor(rgb);
+    const rgb = this.hexToRgbA(colorObject.color)
+    this.changeColor(rgb)
   }
 
   changeColor(arr) {
-    this.trails.selectedR = arr[0];
-    this.trails.selectedG = arr[1];
-    this.trails.selectedB = arr[2];
+    this.trails.selectedR = arr[0]
+    this.trails.selectedG = arr[1]
+    this.trails.selectedB = arr[2]
     if (this.state.cycle === true) {
       setTimeout(() => {
-        const newColor = this.cycleColor(arr[0], arr[1], arr[2]);
-        this.changeColor(newColor);
-      }, 4500);
+        const newColor = this.cycleColor(arr[0], arr[1], arr[2])
+        this.changeColor(newColor)
+      }, 4500)
     }
   }
 
   colorizeImage(event) {
-    const colorize = event.target.checked;
+    const colorize = event.target.checked
     this.setState({
       colorize,
-    });
+    })
 
     if (colorize) {
-      this.trails.colorize = [this.trails.selectedR, this.trails.selectedG, this.trails.selectedB];
+      this.trails.colorize = [this.trails.selectedR, this.trails.selectedG, this.trails.selectedB]
     } else {
-      this.trails.colorize = [];
+      this.trails.colorize = []
     }
   }
 
   mosaicImage(event) {
-    const mosaic = event.target.checked;
+    const mosaic = event.target.checked
     this.setState({
       mosaic,
-    });
-    this.trails.mosaic = mosaic;
+    })
+    this.trails.mosaic = mosaic
   }
 
   changeSize(event) {
-    const size = event.target.value;
-    this.trails.stopDraw();
-    this.trails.pixelSize = size;
+    const size = event.target.value
+    this.trails.stopDraw()
+    this.trails.pixelSize = size
     this.setState(() => {
-      this.changePixels();
-      return { size };
-    });
+      this.changePixels()
+      return { size }
+    })
   }
 
   togglePixel(event) {
-    this.trails.stopDraw();
-    this.setState({ pixelate: event.target.checked });
+    this.trails.stopDraw()
+    this.setState({ pixelate: event.target.checked })
     // TODO: Create Trails.pixelate instead of Trails.mode
     if (event.target.checked === true) {
-      this.trails.mode = 'pixelate';
+      this.trails.mode = 'pixelate'
     } else {
-      this.trails.mode = 'blur';
+      this.trails.mode = 'blur'
     }
-    this.changePixels();
+    this.changePixels()
   }
 
   changePixels() {
-    this.trails.draw();
+    this.trails.draw()
+  }
+
+  cameras() {
+    if (this.state.cameraList.length > 1) {
+      return (
+        <span>
+          <label htmlFor="cameraList">Cameras</label>
+          <select onChange={this.changeCamera.bind(this)}>
+            {this.state.cameraList.map((c, i) => (
+              // console.log(c);
+              <option key={`camera_${i}`} value={i}>
+                {' '}
+                {c.label}{' '}
+              </option>
+            ))}
+          </select>
+        </span>
+      )
+    }
   }
 
   render() {
@@ -205,14 +231,14 @@ export default class GreenScreen extends React.Component {
               checked={this.state.pixelate}
               onChange={this.togglePixel.bind(this)}
             />
-            <label htmlFor="cycle">Cycle</label>
+            {/* <label htmlFor="cycle">Cycle</label>
             <input
               id="cycle"
               name="cycle"
               type="checkbox"
               checked={this.state.cycle}
               onChange={this.cycle.bind(this)}
-            />
+            /> */}
             <label htmlFor="colorize">Colorize</label>
             <input
               id="colorize"
@@ -229,16 +255,7 @@ export default class GreenScreen extends React.Component {
               checked={this.state.mosaic}
               onChange={this.mosaicImage.bind(this)}
             />
-            <label htmlFor="cameraList">Cameras</label>
-            <select onChange={this.changeCamera.bind(this)}>
-              {this.state.cameraList.map((c, i) => (
-                // console.log(c);
-                <option key={`camera_${i}`} value={i}>
-                  {' '}
-                  {c.label}{' '}
-                </option>
-              ))}
-            </select>
+            {this.cameras()}
           </div>
         </div>
         {/* /.navbar */}
@@ -249,7 +266,7 @@ export default class GreenScreen extends React.Component {
               id="videodata"
               loop="loop"
               preload="auto"
-              autoPlay="autoplay"
+              autoPlay
               width={600}
               height={400}
             />
@@ -261,6 +278,6 @@ export default class GreenScreen extends React.Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
